4719e0f1e03164cde753d2be0a89bf77be38c218
Carlos Jasso <contact@carlosjasso.dev>
2022-03-01 17:17:54 -0600

    improved benchmarking timing result

--- tests/helper.py
+++ tests/helper.py
@@ -1,4 +1,4 @@
-from time import perf_counter
+from time import perf_counter_ns
 
 def print_ruled(message: str, new_line: bool = True) -> None:
     ruller = "-" * int((70 - len(message)) / 2)
@@ -6,8 +6,16 @@ def print_ruled(message: str, new_line: bool = True) -> None:
     print(f"{br}{ruller} {message} {ruller}")
 
 def benchmark_start() -> float:
-    return perf_counter()
+    return perf_counter_ns()
 
 def benchmark_stop(start: float) -> None:
-    diff = perf_counter() - start
-    print(f"\ttook: {diff} seconds")
+    diff = perf_counter_ns() - start
+    if diff >= 1_000_000_000:
+        print(f"\ttook: {diff / 1_000_000_000 : .2f} seconds")
+    elif diff >= 1_000_000:
+        print(f"\ttook: {diff / 1_000_000 : .2f} microseconds")
+    elif diff >= 1_000:
+        print(f"\ttook: {diff / 1_000 : .2f} milliseconds")
+    else:
+        print(f"\ttook: {diff : .2f} nanoseconds")
+
